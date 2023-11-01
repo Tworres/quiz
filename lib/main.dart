3342853b4477cf4eb313f9e0b4e7687a9136d69b
Guilherme Torres <36198151+Tworres@users.noreply.github.com>
2023-11-01 01:29:31 -0300

    Reactive Questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(new PerguntaApp());
+  runApp(PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
+class PerguntaAppState extends State<PerguntaApp> {
+  int perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print(perguntaSelecionada);
+  }
+
+  final perguntas = [
+    'pergunta 1',
+    'pergunta 2',
+    'pergunta 3',
+    'pergunta 4',
+    'pergunta 5',
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Primerio aplicativo mobile!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz'),
+        ),
+        body: Column(children: [
+          Text(perguntas[perguntaSelecionada]),
+          ElevatedButton(
+            child: Text('Resposta 1'),
+            onPressed: responder,
+          ),
+          ElevatedButton(
+            child: Text('Resposta 2'),
+            onPressed: responder,
+          )
+        ]),
+      ),
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }

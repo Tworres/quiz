35ff7854360efeb98dab459c8ca4ad894918dca0
Guilherme Torres <36198151+Tworres@users.noreply.github.com>
2023-11-08 23:33:13 -0300

    finish line
    Widget implementation when finish the quiz

--- lib/main.dart
+++ lib/main.dart
@@ -6,42 +6,59 @@ main() {
   runApp(QuizApp());
 }
 
+const List<Map<String, Object>> questions = [
+  {
+    "question": "question 1",
+    "awnsers": ['awnser 1', 'awnser 2', 'awnser 3'],
+  },
+  {
+    "question": "question 2",
+    "awnsers": ['awnser 3', 'awnser 4', 'awnser 5'],
+  },
+  {
+    "question": "question 3",
+    "awnsers": ['awnser 4', 'awnser 5', 'awnser 6']
+  },
+];
+
 class _QuizAppState extends State<QuizApp> {
   int _selectedQuestion = 0;
+  bool _finished = false;
 
+  // EVENT WHEN ANWSER QUESTIONS
   void _respond() {
-    setState(() {
-      _selectedQuestion++;
-    });
+    if ((_selectedQuestion + 1) < questions.length) {
+      setState(() {
+        _selectedQuestion++;
+      });
+    } else {
+      setState(() {
+        _finished = true;
+      });
+    }
     print(_selectedQuestion);
   }
-
-  final List<Map<String, Object>> questions = [
-    {
-      "question": "question 1",
-      "awnsers": ['awnser 1', 'awnser 2', 'awnser 3'],
-    },
-    {
-      "question": "question 2",
-      "awnsers": ['awnser 3', 'awnser 4', 'awnser 5'],
-    },
-    {
-      "question": "question 3",
-      "awnsers": ['awnser 4', 'awnser 5', 'awnser 6']
-    },
-  ];
 
   @override
   Widget build(BuildContext context) {
     List<String> awnsers = questions[_selectedQuestion].cast()['awnsers'];
 
+    // BODY QUESTIONS
+    Widget bodyQuestion = Column(children: [
+      Question(questions[_selectedQuestion]['question'].toString()),
+      ...awnsers.map((e) => Awnser(e, _respond)),
+    ]);
+
+    // BODY FINISHED
+    Widget bodyFinished = const Center(
+      child: Text("Finished", style: TextStyle(fontSize: 28)),
+    );
+
+    // APP
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Quiz')),
-        body: Column(children: [
-          Question(questions[_selectedQuestion]['question'].toString()),
-          ...awnsers.map((e) => Awnser(e, _respond)),
-        ]),
+        body: _finished ? bodyFinished : bodyQuestion,
       ),
     );
   }
